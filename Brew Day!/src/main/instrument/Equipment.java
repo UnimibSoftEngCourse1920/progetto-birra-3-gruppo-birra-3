f6e23ffba05fd7d2a9710b6f0d4bd548df909ccf
Matteo Cusini <m.cusini@campus.unimib.it>
2020-01-02 18:18:38 +0100

    Fix and Add in Equipment fix Equipment add store and createStoreString methods in Equipment

--- Brew Day!/src/main/instrument/Equipment.java
+++ Brew Day!/src/main/instrument/Equipment.java
@@ -3,34 +3,24 @@ package main.instrument;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Scanner;
+import java.sql.*;
 
-import main.DatabaseController;
 
 public class Equipment {
 	
-	private double capacity;
 	private HashMap<String, Double> instruments;
-	private DatabaseController controller;
 	
 	Scanner scan = new Scanner(System.in);
 	
-	public Equipment(HashMap<String, Double> instruments, DatabaseController controller) {
+	public Equipment(HashMap<String, Double> instruments) {
 		super();
 		this.instruments = instruments;
-		this.controller = controller;
-	}
-
-
-	public double getCapacity() {
-		return capacity;
+		
+		// store??
 	}
 
 	public HashMap<String, Double> getInstruments() {
 		return instruments;
-	}
-
-	public void setCapacity(Double capacity) {
-		this.capacity = capacity;
 	}
 
 	public void setInstruments(HashMap<String, Double> instruments) {
@@ -47,12 +37,12 @@ public class Equipment {
 	}
 	
 	// let change the capacity value of every instrument
-	public void updateInstruments(HashMap<String, Double> instruments) {
+	public void updateInstruments() {
 		
 		for (Entry<String, Double> i : instruments.entrySet()) {
 		    System.out.println("Do you want to change the value of " + i.getKey() + " ?");
 		    
-		    // scanning char Y: yes N: no
+		    // scanning char Y,y: yes; N,n: no
 		    char c = scan.next().charAt(0);
 		    
 		    if (c == 'Y' || c == 'y') { // positive answer
@@ -65,10 +55,53 @@ public class Equipment {
 		    else if (c == 'N' || c == 'n')
 		    	System.out.println("Beautiful");
 		}
+		
+		store(instruments);
 	}
 	
-	public void store() {
-		this.controller.store();
+	public void store(HashMap<String, Double> instruments) {
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			//Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//Open a connection
+			System.out.println("Connecting to DB...");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/", "username", "password");
+			System.out.println("Connected DB successfully...");
+			
+			//Execute a query
+			System.out.println("Creating statement...");
+		    stmt = conn.createStatement();
+		    String sql = createStoreString(instruments);
+		    stmt.executeUpdate(sql);
+
+		}catch(SQLException se){
+			//Handle errors for JDBC
+			se.printStackTrace();
+		}catch(Exception e){
+			//Handle errors for Class.forName
+			e.printStackTrace();
+		}finally{
+			
+			try{
+				if(stmt!=null)
+					stmt.close();
+			}catch(SQLException se2){
+			}
+			try{
+				if(conn!=null)
+					conn.close();
+			}catch(SQLException se){
+				se.printStackTrace();
+			}
+		}
+	}
+	
+	public String createStoreString(HashMap<String, Double> instruments) {
+		//creating script SQL for store instruments in DB
+		return null;
 	}
 	
 }
